09e5dff89d80ddec09ee5baebfc2ff32b2725208
Kar Rui Lau <karrui@open.gov.sg>
2025-02-13 16:00:30 +0800

    feat: add SkipNavLink component

---
+++ packages/components/src/skip-nav-link/skip-nav-link.tsx
@@ -0,0 +1,21 @@
+import { AnchorHTMLAttributes } from "react"
+import { skipNavLinkStyles, VariantProps } from "@opengovsg/oui-theme"
+
+interface SkipNavLinkProps
+  extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "id" | "href">,
+    VariantProps<typeof skipNavLinkStyles> {
+  /**
+   * The id of the element to skip to
+   */
+  id: string
+}
+
+export const SkipNavLink = ({ className, id, ...props }: SkipNavLinkProps) => {
+  return (
+    <a
+      className={skipNavLinkStyles({ className })}
+      {...props}
+      href={`#${id}`}
+    />
+  )
+}
--- packages/theme/src/components/index.ts
+++ packages/theme/src/components/index.ts
@@ -3,4 +3,5 @@ export * from "./spinner"
 export * from "./calendar"
 export * from "./govt-banner"
 export * from "./toggle"
+export * from "./skip-nav-link"
 // Add new theme exports here
