9d51e75f9e7f34898c2bdeb54110f09e7eeb3c1e
Kar Rui Lau <karrui@open.gov.sg>
2025-02-14 14:48:34 +0800

    feat: add color mode toggle

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 
+import { cookies } from "next/headers"
 import { docsConfig } from "@/config/docs.config"
 
 const geistSans = Geist({
@@ -23,15 +24,30 @@ export const metadata: Metadata = {
   description: docsConfig.description,
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const storedTheme = (await cookies()).get("theme")?.value
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                if (${storedTheme === "dark"} || (${!storedTheme} && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                }
+              })();
+            `,
+          }}
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-white text-black antialiased dark:bg-black dark:text-white`}
       >
         {children}
       </body>
