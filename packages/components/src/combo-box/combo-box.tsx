99b63297bde70ab91522b64ed862622d432524bd
Kar Rui Lau <karrui@open.gov.sg>
2025-02-18 16:22:24 +0800

    feat(ComboBox): add initial defaults and logic

--- packages/components/src/combo-box/combo-box.tsx
+++ packages/components/src/combo-box/combo-box.tsx
@@ -1,11 +1,89 @@
-import { comboBoxStyles, VariantProps } from "@opengovsg/oui-theme"
+import { useMemo } from "react"
+import { cn, comboBoxStyles, VariantProps } from "@opengovsg/oui-theme"
+import { Button } from "@opengovsg/oui/button"
+import {
+  ComboBox as AriaComboBox,
+  ComboBoxProps as AriaComboBoxProps,
+  FieldError,
+  Input,
+  Label,
+  ListBox,
+  ListBoxItem,
+  ListBoxItemProps,
+  Popover,
+  Text,
+  UNSTABLE_ListLayout,
+  UNSTABLE_Virtualizer,
+  ValidationResult,
+} from "react-aria-components"
 
-interface ComboBoxProps extends VariantProps<typeof comboBoxStyles> {}
+export interface ComboBoxProps<
+  T extends { value: string; name: string } = { value: string; name: string },
+> extends VariantProps<typeof comboBoxStyles>,
+    Omit<AriaComboBoxProps<T>, "children"> {
+  label?: string
+  description?: string | null
+  errorMessage?: string | ((validation: ValidationResult) => string)
+  items: T[]
+}
 
-export const ComboBox = ({  }: ComboBoxProps) => {
+export function ComboBox<T extends { value: string; name: string }>({
+  label,
+  description,
+  errorMessage,
+  items,
+  ...props
+}: ComboBoxProps<T>) {
+  const layout = useMemo(() => {
+    return new UNSTABLE_ListLayout({
+      rowHeight: 25,
+    })
+  }, [])
+
   return (
-    <div>
-      <h1>combo-box</h1>
-    </div>
+    <AriaComboBox {...props}>
+      {({ isOpen }) => (
+        <>
+          <Label>{label}</Label>
+          <div className="my-combobox-container">
+            <Input />
+            <Button>{isOpen ? "▲" : "▼"}</Button>
+          </div>
+          {description && <Text slot="description">{description}</Text>}
+          <FieldError>{errorMessage}</FieldError>
+          <UNSTABLE_Virtualizer layout={layout}>
+            <Popover className="w-[var(--trigger-width)]">
+              {({ trigger }) => {
+                console.log("trigger", trigger)
+                return (
+                  <ListBox
+                    className={cn(
+                      "block max-h-[300px] min-h-[100px] w-[250px] overflow-y-auto",
+                      trigger === "ComboBox" && "w-[unset]",
+                    )}
+                    items={items}
+                  >
+                    {(item) => (
+                      <ComboBoxItem id={item.value}>{item.name}</ComboBoxItem>
+                    )}
+                  </ListBox>
+                )
+              }}
+            </Popover>
+          </UNSTABLE_Virtualizer>
+        </>
+      )}
+    </AriaComboBox>
+  )
+}
+
+export function ComboBoxItem(props: ListBoxItemProps) {
+  return (
+    <ListBoxItem
+      {...props}
+      className={({ isFocused, isSelected }) =>
+        cn(isFocused && "bg-gray-200", isSelected && "bg-gray-300")
+      }
+    />
   )
 }
--- packages/components/src/combo-box/stories/combo-box.stories.tsx
+++ packages/components/src/combo-box/stories/combo-box.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { ComboBox } from "../combo-box"
+import type { Key } from "react-aria-components"
+import { useState } from "react"
+
+import { ComboBox, ComboBoxProps } from "../combo-box"
 
 export default {
   title: "Components/ComboBox",
@@ -9,5 +12,69 @@ export default {
 type Story = StoryObj<typeof ComboBox>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    label: "Ice cream flavour",
+    items: [...Array(2000)].map((_, i) => ({
+      value: String(i),
+      name: `Item ${i}`,
+    })),
+  },
+}
+
+const ControlledComboBoxTemplate = ({ items }: ComboBoxProps) => {
+  const [fieldState, setFieldState] = useState<{
+    selectedKey: Key | null
+    inputValue: string
+  }>({
+    selectedKey: null,
+    inputValue: "",
+  })
+
+  const onSelectionChange = (id: Key | null) => {
+    setFieldState({
+      inputValue: items.find((o) => o.value === id)?.name ?? "",
+      selectedKey: id,
+    })
+  }
+
+  const onInputChange = (value: string) => {
+    setFieldState((prevState) => ({
+      inputValue: value,
+      selectedKey: value === "" ? null : prevState.selectedKey,
+    }))
+  }
+
+  return (
+    <>
+      <p>Current selected major id: {fieldState.selectedKey}</p>
+      <p>Current input text: {fieldState.inputValue}</p>
+      <ComboBox
+        label="Pick a engineering major"
+        items={items}
+        selectedKey={fieldState.selectedKey}
+        inputValue={fieldState.inputValue}
+        onSelectionChange={onSelectionChange}
+        onInputChange={onInputChange}
+      />
+    </>
+  )
+}
+
+export const FullyControlled: Story = {
+  render(args) {
+    return <ControlledComboBoxTemplate {...args} />
+  },
+  args: {
+    items: [
+      { value: String(1), name: "Aerospace" },
+      { value: String(2), name: "Mechanical" },
+      { value: String(3), name: "Civil" },
+      { value: String(4), name: "Biomedical" },
+      { value: String(5), name: "Nuclear" },
+      { value: String(6), name: "Industrial" },
+      { value: String(7), name: "Chemical" },
+      { value: String(8), name: "Agricultural" },
+      { value: String(9), name: "Electrical" },
+    ],
+  },
 }
