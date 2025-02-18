baf70591af2dccc87e0c08cebb212746690cbc35
Kar Rui Lau <karrui@open.gov.sg>
2025-02-18 18:05:10 +0800

    fix: a11y

--- packages/components/src/text-area/stories/text-area.stories.tsx
+++ packages/components/src/text-area/stories/text-area.stories.tsx
@@ -1,9 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react"
+
 import { TextArea } from "../text-area"
 
 export default {
   title: "Components/TextArea",
   component: TextArea,
+  args: {
+    placeholder:
+      "Remember to have a placeholder or aria-label for this component to be accessible",
+  },
 } as Meta<typeof TextArea>
 
 type Story = StoryObj<typeof TextArea>
