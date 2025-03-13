929c4369d80f464f69465388014cd01dcb17c890
Kar Rui Lau <karrui@open.gov.sg>
2025-03-13 21:47:31 +0800

    feat(story): add UnavailableDates story

--- packages/components/src/calendar/stories/calendar.stories.tsx
+++ packages/components/src/calendar/stories/calendar.stories.tsx
@@ -1,4 +1,8 @@
+import type { DateValue } from "@react-aria/calendar"
 import type { Meta, StoryObj } from "@storybook/react"
+import { useCallback } from "react"
+import { isWeekend } from "@internationalized/date"
+import { useLocale } from "react-aria"
 
 import { Calendar } from "../calendar"
 
@@ -22,5 +26,23 @@ export const Default: Story = {}
 export const Sizes: Story = {
   args: {
     size: "sm",
+  },
+}
+
+export const UnavailableDates: Story = {
+  render: (args) => {
+    const { locale } = useLocale()
+    const isDateUnavailable = useCallback(
+      (date: DateValue) => isWeekend(date, locale),
+      [locale],
+    )
+
+    return (
+      <Calendar
+        aria-label="Appointment date"
+        {...args}
+        isDateUnavailable={isDateUnavailable}
+      />
+    )
   },
 }
