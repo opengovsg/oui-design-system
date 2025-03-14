718776eefc91aa1b3866ca672240c27f2e72c5bf
Kar Rui Lau <karrui@open.gov.sg>
2025-03-14 11:16:30 +0800

    fix: correct return type for createContext

--- packages/components/src/system/react-utils/context.ts
+++ packages/components/src/system/react-utils/context.ts
@@ -19,11 +19,7 @@ export interface CreateContextOptions {
   name?: string
 }
 
-export type CreateContextReturn<T> = [
-  React.Provider<T>,
-  () => T,
-  React.Context<T>,
-]
+export type CreateContextReturn<T> = [React.Context<T>, () => T]
 
 /**
  * Creates a named context, provider, and hook.
@@ -57,9 +53,5 @@ export function createContext<ContextType>(
     return context
   }
 
-  return [
-    Context.Provider,
-    useContext,
-    Context,
-  ] as CreateContextReturn<ContextType>
+  return [Context, useContext] as CreateContextReturn<ContextType>
 }
