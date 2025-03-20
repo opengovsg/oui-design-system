2d13edf90aebc164735afd098cfec2277b68c958
Kar Rui Lau <karrui@open.gov.sg>
2025-03-20 12:42:46 +0800

    feat(select): update icon to only be chevron down

--- packages/components/src/select/select.tsx
+++ packages/components/src/select/select.tsx
@@ -6,7 +6,7 @@ import type {
   ListLayoutOptions,
 } from "react-aria-components"
 import { useMemo } from "react"
-import { ChevronsUpDownIcon } from "lucide-react"
+import { ChevronDownIcon } from "lucide-react"
 import {
   Select as AriaSelect,
   ListBox,
@@ -114,7 +114,12 @@ export function Select<T extends object>({
               className: classNames?.selectedText,
             })}
           />
-          <ChevronsUpDownIcon className="h-4 w-4" />
+
+          <ChevronDownIcon
+            className={styles.icon({
+              className: classNames?.icon,
+            })}
+          />
         </Button>
         {description && (
           <Description
--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -13,8 +13,8 @@ export const selectSearchStyles = tv({
 
 export const selectItemStyles = tv({
   slots: {
-    base: "text-base-content-strong group flex cursor-default items-center gap-2 rounded-sm px-4 py-2 outline-hidden select-none",
-    text: "text-base-content-strong line-clamp-1 flex-1",
+    base: "text-base-content-strong outline-hidden group flex cursor-default select-none items-center gap-2 rounded-sm px-4 py-2",
+    text: "line-clamp-1 flex-1 text-inherit",
   },
   variants: {
     color: {
@@ -58,9 +58,10 @@ export const selectStyles = tv({
     base: "group flex flex-col gap-1",
     label: "",
     trigger: "w-fit",
-    selectedText: "flex-1 truncate text-start",
+    icon: "size-4",
+    selectedText: "text-base-content-strong flex-1 truncate text-start",
     popover:
-      "motion-safe:entering:animate-in motion-safe:entering:fade-in motion-safe:exiting:animate-out motion-safe:exiting:fade-out flex w-(--trigger-width) flex-col rounded-sm bg-white shadow-md",
+      "motion-safe:entering:animate-in motion-safe:entering:fade-in motion-safe:exiting:animate-out motion-safe:exiting:fade-out w-(--trigger-width) flex flex-col rounded-sm bg-white shadow-md",
     list: "overflow-y-auto",
     description: "",
   },
