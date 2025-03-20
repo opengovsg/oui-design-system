43320a8a2d3ca0062375106c701c4aa6778a0ada
Kar Rui Lau <karrui@open.gov.sg>
2025-03-20 17:17:59 +0800

    feat: overflow on calendar instead of base

--- packages/theme/src/components/calendar.ts
+++ packages/theme/src/components/calendar.ts
@@ -5,7 +5,7 @@ import { tv } from "../utils/tv"
 
 export const calendarStyles = tv({
   slots: {
-    base: "relative inline-block h-fit max-w-full overflow-x-auto overflow-y-hidden rounded-sm shadow-sm",
+    base: "relative inline-block h-fit max-w-full overflow-y-hidden rounded-sm shadow-sm",
     prevButton: "",
     nextButton: "",
     buttonGroup: "justify-self-end",
@@ -19,7 +19,7 @@ export const calendarStyles = tv({
     ],
     grid: "",
     calendar: "",
-    gridWrapper: "flex gap-x-4 max-sm:block",
+    gridWrapper: "flex gap-x-4 overflow-x-auto max-sm:block",
     gridHeader: "",
     gridHeaderCell: "text-base-content-default",
     gridBody: "",
