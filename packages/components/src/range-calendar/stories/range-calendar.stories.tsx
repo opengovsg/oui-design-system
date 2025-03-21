d7c97103f57f064138e37f54a24e3eb6f2df658d
Kar Rui Lau <karrui@open.gov.sg>
2025-03-21 17:16:22 +0800

    feat: add RangeCalendar stories

--- packages/components/src/range-calendar/stories/range-calendar.stories.tsx
+++ packages/components/src/range-calendar/stories/range-calendar.stories.tsx
@@ -1,5 +1,10 @@
+import type { DateValue } from "@react-aria/calendar"
 import type { Meta, StoryObj } from "@storybook/react"
-import { CalendarDate } from "@internationalized/date"
+import { useCallback } from "react"
+import { CalendarDate, isWeekend } from "@internationalized/date"
+import { withChromaticModes } from "@oui/chromatic"
+import { userEvent } from "@storybook/test"
+import { useLocale } from "react-aria"
 
 import { RangeCalendar } from "../range-calendar"
 
@@ -21,6 +26,71 @@ export default {
 
 type Story = StoryObj<typeof RangeCalendar>
 
-export const Default: Story = {
-  args: {},
+export const Default: Story = {}
+
+export const Disabled: Story = {
+  args: { isDisabled: true },
+}
+
+export const MultipleMonths: Story = {
+  args: {
+    visibleDuration: { months: 2 },
+    defaultValue: {
+      start: new CalendarDate(2025, 3, 13),
+      end: new CalendarDate(2025, 4, 20),
+    },
+  },
+  parameters: {
+    chromatic: withChromaticModes(["desktop", "mobileSmall"]),
+  },
+}
+
+export const MediumSizeMultipleMonths: Story = {
+  args: {
+    size: "md",
+    visibleDuration: { months: 2 },
+  },
+}
+
+export const UnavailableDates: Story = {
+  render: (args) => {
+    const { locale } = useLocale()
+    const isDateUnavailable = useCallback(
+      (date: DateValue) => isWeekend(date, locale),
+      [locale],
+    )
+
+    return (
+      <RangeCalendar
+        aria-label="Appointment date"
+        {...args}
+        isDateUnavailable={isDateUnavailable}
+      />
+    )
+  },
+  play: async ({ canvas }) => {
+    userEvent.click(
+      canvas.getByRole("button", {
+        name: /wednesday, 12 march 2025/i,
+      }),
+    )
+  },
+}
+
+export const CustomFirstDayOfWeek: Story = {
+  args: {
+    firstDayOfWeek: "fri",
+  },
+}
+
+export const HideTodayButton: Story = {
+  args: { showTodayButton: false },
+}
+
+export const HideOutsideMonths: Story = {
+  args: {
+    classNames: {
+      cell: "outside-month:hidden",
+    },
+  },
 }
