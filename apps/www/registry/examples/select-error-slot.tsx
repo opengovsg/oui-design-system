78fee41179eec1b2967696405cef1e901827b277
Kar Rui Lau <karrui@open.gov.sg>
2025-04-01 15:14:24 +0800

    docs: add select error slot example

--- apps/www/registry/examples/select-error-slot.tsx
+++ apps/www/registry/examples/select-error-slot.tsx
@@ -1,23 +1,45 @@
 "use client"
 
+import type { Key } from "react-aria-components"
+import { useState } from "react"
+
 import { Select, SelectItem } from "@opengovsg/oui"
 
-export default function SelectDescription() {
-  const options = [
-    { id: 1, textValue: "Aerospace" },
-    { id: 2, textValue: "Mechanical" },
-    { id: 3, textValue: "Civil" },
-    { id: 4, textValue: "Biomedical" },
-    { id: 5, textValue: "Nuclear" },
-    { id: 6, textValue: "Industrial" },
-    { id: 7, textValue: "Chemical" },
-    { id: 8, textValue: "Agricultural" },
-    { id: 9, textValue: "Electrical" },
-  ]
+export const animals = [
+  { id: "cat", textValue: "Cat" },
+  { id: "dog", textValue: "Dog" },
+  { id: "elephant", textValue: "Elephant" },
+  { id: "lion", textValue: "Lion" },
+  { id: "tiger", textValue: "Tiger" },
+  { id: "giraffe", textValue: "Giraffe" },
+  { id: "dolphin", textValue: "Dolphin" },
+  { id: "penguin", textValue: "Penguin" },
+  { id: "zebra", textValue: "Zebra" },
+  { id: "shark", textValue: "Shark" },
+  { id: "whale", textValue: "Whale" },
+  { id: "otter", textValue: "Otter" },
+  { id: "crocodile", textValue: "Crocodile" },
+]
+
+export default function App() {
+  const [value, setValue] = useState<Key | null>(null)
+  const isValid = value === "cat"
 
   return (
-    <Select label="Engineering" items={options} isInvalid>
-      {(item) => <SelectItem>{item.textValue}</SelectItem>}
+    <Select
+      description="The second most popular pet in the world"
+      errorMessage={isValid ? "" : "You must select a cat"}
+      isInvalid={!isValid}
+      label="Favorite Animal"
+      placeholder="Select an animal"
+      selectedKey={value}
+      onSelectionChange={setValue}
+    >
+      {animals.map((animal) => (
+        <SelectItem key={animal.id} id={animal.id}>
+          {animal.textValue}
+        </SelectItem>
+      ))}
     </Select>
   )
 }
