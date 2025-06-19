f90cf62a2c42ea1cfa5d33561f0eae9e8dcfa3e9
Kar Rui Lau <karrui@open.gov.sg>
2025-06-19 12:54:18 +0800

    feat(date-picker): add initial styling for date picker

--- packages/components/src/date-field/date-field.tsx
+++ packages/components/src/date-field/date-field.tsx
@@ -1,13 +1,83 @@
 "use client"
 
-import { dateFieldStyles, VariantProps } from "@opengovsg/oui-theme"
+import {
+  DateField as AriaDateField,
+  DateFieldProps as AriaDateFieldProps,
+  DateInput as AriaDateInput,
+  DateInputProps,
+  DateSegment,
+  DateValue,
+  Label,
+  ValidationResult,
+} from "react-aria-components"
 
-interface DateFieldProps extends VariantProps<typeof dateFieldStyles> {}
+import {
+  composeTailwindRenderProps,
+  dateFieldStyles,
+  fieldGroupStyles,
+  tv,
+  VariantProps,
+} from "@opengovsg/oui-theme"
 
-export const DateField = ({  }: DateFieldProps) => {
+import { Description, FieldError } from "../field"
+import { mapPropsVariants } from "../system/utils"
+
+interface DateFieldProps<T extends DateValue>
+  extends AriaDateFieldProps<T>,
+    VariantProps<typeof dateFieldStyles> {
+  label?: string
+  description?: string
+  errorMessage?: string | ((validation: ValidationResult) => string)
+}
+
+export function DateField<T extends DateValue>(
+  originalProps: DateFieldProps<T>,
+) {
+  const [{ label, description, errorMessage, ...props }, variantProps] =
+    mapPropsVariants(originalProps, dateFieldStyles.variantKeys)
   return (
-    <div>
-      <h1>date-field</h1>
-    </div>
+    <AriaDateField
+      {...props}
+      className={composeTailwindRenderProps(
+        props.className,
+        "flex flex-col gap-1",
+      )}
+    >
+      {label && <Label>{label}</Label>}
+      <DateInput />
+      {description && <Description>{description}</Description>}
+      <FieldError>{errorMessage}</FieldError>
+    </AriaDateField>
+  )
+}
+
+const segmentStyles = tv({
+  base: "type-literal:px-0 rounded-xs inline p-0.5 text-gray-800 caret-transparent outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]",
+  variants: {
+    isPlaceholder: {
+      true: "italic text-gray-600 dark:text-zinc-400",
+    },
+    isDisabled: {
+      true: "text-gray-200 dark:text-zinc-600 forced-colors:text-[GrayText]",
+    },
+    isFocused: {
+      true: "bg-blue-600 text-white dark:text-white forced-colors:bg-[Highlight] forced-colors:text-[HighlightText]",
+    },
+  },
+})
+
+export function DateInput(props: Omit<DateInputProps, "children">) {
+  return (
+    <AriaDateInput
+      className={(renderProps) =>
+        fieldGroupStyles({
+          ...renderProps,
+          class: "block min-w-[150px] px-2 py-1.5 text-sm",
+        })
+      }
+      {...props}
+    >
+      {(segment) => <DateSegment segment={segment} className={segmentStyles} />}
+    </AriaDateInput>
   )
 }
--- packages/components/src/date-picker/date-picker.tsx
+++ packages/components/src/date-picker/date-picker.tsx
@@ -1,13 +1,66 @@
 "use client"
 
-import { datePickerStyles, VariantProps } from "@opengovsg/oui-theme"
+import { CalendarIcon } from "lucide-react"
+import {
+  DatePicker as AriaDatePicker,
+  DatePickerProps as AriaDatePickerProps,
+  DateSegment,
+  DateValue,
+  Dialog,
+  DialogTrigger,
+  Group,
+  ValidationResult,
+} from "react-aria-components"
 
-interface DatePickerProps extends VariantProps<typeof datePickerStyles> {}
+import {
+  composeTailwindRenderProps,
+  datePickerStyles,
+  VariantProps,
+} from "@opengovsg/oui-theme"
 
-export const DatePicker = ({  }: DatePickerProps) => {
+import { Button } from "../button"
+import { Calendar } from "../calendar"
+import { DateInput } from "../date-field"
+import { Description, FieldError, FieldGroup, Label } from "../field"
+import { Popover } from "../popover"
+
+interface DatePickerProps<T extends DateValue>
+  extends VariantProps<typeof datePickerStyles>,
+    AriaDatePickerProps<T> {
+  label?: string
+  description?: string
+  errorMessage?: string | ((validation: ValidationResult) => string)
+}
+
+export function DatePicker<T extends DateValue>({
+  label,
+  description,
+  errorMessage,
+  ...props
+}: DatePickerProps<T>) {
   return (
-    <div>
-      <h1>date-picker</h1>
-    </div>
+    <AriaDatePicker
+      {...props}
+      className={composeTailwindRenderProps(
+        props.className,
+        "",
+        // "group flex flex-col gap-1",
+      )}
+    >
+      {label && <Label>{label}</Label>}
+      <FieldGroup className="w-auto min-w-[208px]">
+        <DateInput className="min-w-[150px] flex-1 px-2 py-1.5 text-sm" />
+        <Button isIconOnly className="rounded-xs mr-1 w-6 outline-offset-0">
+          <CalendarIcon aria-hidden className="h-4 w-4" />
+        </Button>
+      </FieldGroup>
+      <Popover placement="bottom end">
+        <Dialog>
+          <Calendar />
+        </Dialog>
+      </Popover>
+      {description && <Description>{description}</Description>}
+      <FieldError>{errorMessage}</FieldError>
+    </AriaDatePicker>
   )
 }
