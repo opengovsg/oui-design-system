81ba33cec631ea93ae875433ef26ab38cd3b87d0
Kar Rui Lau <karrui@open.gov.sg>
2025-08-08 17:14:44 +0800

    feat: update storybook packages to v9.1.1

--- apps/storybook/eslint.config.mjs
+++ apps/storybook/eslint.config.mjs
@@ -1,13 +1,13 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config as baseConfig } from "@oui/eslint-config/storybook"
 
 /**
  * @type {import("eslint").Linter.Config}
  */
-const config = [
-  ...baseConfig,
-  {
-    ignores: ["!.storybook/*", "storybook-static"],
-  },
-]
+const config = [...baseConfig, {
+  ignores: ["!.storybook/*", "storybook-static"],
+}, ...storybook.configs["flat/recommended"]]
 
 export default config
