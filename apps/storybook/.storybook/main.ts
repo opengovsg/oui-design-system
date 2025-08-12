034ea1435aabf0e1ef82e626f1b5faa4e34c25c5
Kar Rui Lau <karrui@open.gov.sg>
2025-08-12 11:59:19 +0800

    fix(storybook): try removing getAbsolutePath from config
    chromatic really throwing build errors

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,4 +1,3 @@
-import { dirname, join } from "path"
 import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
@@ -8,14 +7,14 @@ const config: StorybookConfig = {
     "../../../packages/theme/stories/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest"),
-    getAbsolutePath("@storybook/addon-themes"),
-    getAbsolutePath("@storybook/addon-docs"),
+    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest",
+    "@storybook/addon-themes",
+    "@storybook/addon-docs",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
 
@@ -37,7 +36,3 @@ const config: StorybookConfig = {
 }
 
 export default config
-
-function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")))
-}
