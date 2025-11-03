7a4531a091588e83b4281a196fd9f617a9554bc3
Kar Rui Lau <karrui@open.gov.sg>
2025-11-03 17:03:39 +0800

    fix(www): update skip-nav-link to show properly

--- packages/theme/src/components/skip-nav-link.ts
+++ packages/theme/src/components/skip-nav-link.ts
@@ -3,7 +3,7 @@ import type { VariantProps } from "tailwind-variants"
 import { tv } from "../utils/tv"
 
 export const skipNavLinkStyles = tv({
-  base: "sr-only inline-flex rounded-md p-10 text-sm font-semibold select-none focus:not-sr-only",
+  base: "sr-only inline-flex rounded-md p-10 text-sm font-semibold select-none focus:not-sr-only focus:absolute focus:z-100",
 })
 
 export type SkipNavLinkVariantProps = VariantProps<typeof skipNavLinkStyles>
