5c53ade05003d05c213d6abbe4ae0c7014072c85
Kar Rui Lau <karrui@open.gov.sg>
2026-01-09 14:46:33 +0800

    fix(www): update example to use prop on accordion

--- apps/www/registry/examples/accordion-default-expanded.tsx
+++ apps/www/registry/examples/accordion-default-expanded.tsx
@@ -9,15 +9,15 @@ import {
 
 export default function AccordionDefaultExpanded() {
   return (
-    <Accordion>
-      <AccordionItem defaultExpanded>
+    <Accordion defaultExpandedKeys={["item-1"]}>
+      <AccordionItem id="item-1">
         <AccordionHeader>This item is expanded by default</AccordionHeader>
         <AccordionContent>
           You can set an accordion item to be expanded by default using the
           defaultExpanded prop.
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem>
+      <AccordionItem id="item-2">
         <AccordionHeader>This item is collapsed by default</AccordionHeader>
         <AccordionContent>
           This content is hidden until you click on the header.
