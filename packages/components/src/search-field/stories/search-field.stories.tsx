e444bd0ddb7fd12019359966d567541e06a143eb
Kar Rui Lau <karrui@open.gov.sg>
2026-02-10 16:30:13 +0800

    feat: add story to show submit value distinct from input value

--- packages/components/src/search-field/stories/search-field.stories.tsx
+++ packages/components/src/search-field/stories/search-field.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite"
+import { useState } from "react"
+import { expect } from "storybook/test"
 
 import { SearchField } from "../search-field"
 
@@ -60,5 +62,50 @@ export const Sizes: Story = {
   },
   args: {
     label: "Date of issue",
+  },
+}
+
+export const Controlled: Story = {
+  render() {
+    const [value, setValue] = useState("")
+    const [submittedValue, setSubmittedValue] = useState<string | null>(null)
+
+    return (
+      <div className="space-y-4">
+        <SearchField
+          label="Controlled SearchField"
+          value={value}
+          onChange={(v) => setValue(v)}
+          onSubmit={() => setSubmittedValue(value)}
+        />
+        <p className="text-sm text-gray-700">
+          <div>
+            Input value: <strong data-testid="input-value">{value}</strong>
+          </div>
+          <div>
+            Submitted value:{" "}
+            <strong data-testid="submitted-value">{submittedValue}</strong>
+          </div>
+        </p>
+      </div>
+    )
+  },
+  play: async ({ userEvent, canvas }) => {
+    const TEST_SEARCH_TEXT = "Type anything, press enter to submit"
+    const inputValue = canvas.getByTestId("input-value")
+    const submittedValue = canvas.getByTestId("submitted-value")
+
+    // Verify initial state
+    await expect(inputValue).toHaveTextContent("")
+    await expect(submittedValue).toHaveTextContent("")
+
+    // Simulate user typing and submitting the form
+    const searchField = canvas.getByLabelText("Controlled SearchField")
+    await userEvent.type(searchField, TEST_SEARCH_TEXT)
+    await userEvent.keyboard("{Enter}")
+
+    // Verify updated state
+    await expect(inputValue).toHaveTextContent(TEST_SEARCH_TEXT)
+    await expect(submittedValue).toHaveTextContent(TEST_SEARCH_TEXT)
   },
 }
