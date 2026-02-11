82340edec0e60f6ef4b75ad61d4be71a6d067a71
Kar Rui Lau <karrui@open.gov.sg>
2026-02-11 14:11:11 +0800

    docs: add space in pseudo

--- apps/www/registry/examples/link-with-pseudo-elements.tsx
+++ apps/www/registry/examples/link-with-pseudo-elements.tsx
@@ -6,7 +6,7 @@ import { Link } from "@opengovsg/oui"
 
 export default function LinkWithPseudoElements() {
   return (
-    <Link href="#" className="before:content-['→'] after:content-['↗']">
+    <Link href="#" className="before:content-['→_'] after:content-['_↗']">
       See code for this example
     </Link>
   )
