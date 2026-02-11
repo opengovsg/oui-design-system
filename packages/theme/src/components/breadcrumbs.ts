f3935036d0bf6ffdf54d5bf9b421888ca6456cc7
Kar Rui Lau <karrui@open.gov.sg>
2026-02-11 16:19:15 +0800

    feat: add whitespace-pre on breadcrumb crumb

--- packages/theme/src/components/breadcrumbs.ts
+++ packages/theme/src/components/breadcrumbs.ts
@@ -3,7 +3,7 @@ import { tv } from "../utils/tv"
 export const breadcrumbsStyles = tv({
   slots: {
     base: "flex gap-1",
-    crumb: "flex items-center gap-1",
+    crumb: "flex items-center gap-1 whitespace-pre",
     link: "disabled:text-base-content-default inline-flex items-center justify-center *:h-[1em]",
     separator:
       "text-interaction-support-disabled-content inline-flex h-[1em] w-fit items-center justify-center leading-none",
