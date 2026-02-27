1c4c90d3d83cc90af6c8bffab535432565c4828f
Kar Rui Lau <karrui@open.gov.sg>
2026-02-27 10:54:58 +0800

    fix(tooltip): use styles.arrow

--- packages/components/src/tooltip/tooltip.tsx
+++ packages/components/src/tooltip/tooltip.tsx
@@ -63,7 +63,10 @@ export function Tooltip(originalProps: TooltipProps) {
                 width={8}
                 height={8}
                 viewBox="0 0 8 8"
-                className={renderChildren(renderProps, classNames?.arrow)}
+                className={styles.arrow({
+                  ...renderProps,
+                  className: renderChildren(renderProps, classNames?.arrow),
+                })}
               >
                 <path d="M0 0 L4 4 L8 0" />
               </svg>
