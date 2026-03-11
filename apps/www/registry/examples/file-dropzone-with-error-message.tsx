2d7374998d95d592635795b9196c284cbc78ca4d
Kar Rui Lau <karrui@open.gov.sg>
2026-03-11 12:26:50 +0800

    docs(file-dropzone): update error handling example to use toast

--- apps/www/registry/examples/file-dropzone-with-error-message.tsx
+++ apps/www/registry/examples/file-dropzone-with-error-message.tsx
@@ -1,21 +1,14 @@
 "use client"
 
-import { useState } from "react"
-
-import { FileDropzone } from "@opengovsg/oui"
+import { FileDropzone, toast } from "@opengovsg/oui"
 
 export default function FileDropzoneWithErrorMessage() {
-  const [error, setError] = useState<string>("")
-
   return (
     <FileDropzone
       label="Upload PDF only"
       allowedMimeTypes={["application/pdf"]}
       maxFileSize={1000 * 1000} // 1MB
-      onChange={() => setError("")}
-      onError={setError}
-      isInvalid={!!error}
-      errorMessage={error}
+      onError={(error) => toast.error(error)}
     />
   )
 }
