958223564ad765c2ca943aaa9d354c08e8dba5fc
Kar Rui Lau <karrui@open.gov.sg>
2026-03-11 12:29:32 +0800

    Revert "docs(file-dropzone): update error handling example to use toast"
    This reverts commit 2d7374998d95d592635795b9196c284cbc78ca4d.

--- apps/www/registry/examples/file-dropzone-with-error-message.tsx
+++ apps/www/registry/examples/file-dropzone-with-error-message.tsx
@@ -1,14 +1,21 @@
 "use client"
 
-import { FileDropzone, toast } from "@opengovsg/oui"
+import { useState } from "react"
+
+import { FileDropzone } from "@opengovsg/oui"
 
 export default function FileDropzoneWithErrorMessage() {
+  const [error, setError] = useState<string>("")
+
   return (
     <FileDropzone
       label="Upload PDF only"
       allowedMimeTypes={["application/pdf"]}
       maxFileSize={1000 * 1000} // 1MB
-      onError={(error) => toast.error(error)}
+      onChange={() => setError("")}
+      onError={setError}
+      isInvalid={!!error}
+      errorMessage={error}
     />
   )
 }
